e5ab37dd771e2eeb3d9bc100f455c0799c7adb95
Sunguk Ham <ham2959@gmail.com>
2017-04-05 14:15:05 -0700

    april 5th

---
+++ Lab10/src/lab10/Foo.java
@@ -0,0 +1,15 @@
+package lab10;
+
+import java.util.Scanner;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        for (int i = 0; i < args.length; i++) {
+//            
+//        }
+        args[1] = scan.next();
+    }
+    
+}
--- assignment 4/COMP1510-assign-4-template/COMP1510-assign-4-template/src/q3/StopWatchPanel.java
+++ assignment 4/COMP1510-assign-4-template/COMP1510-assign-4-template/src/q3/StopWatchPanel.java
@@ -2,31 +2,47 @@ package q3;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.Timer;
+import java.text.DecimalFormat;
 import javax.swing.JPanel;
 
 public class StopWatchPanel extends JPanel{
     
+    DecimalFormat fmt = new DecimalFormat("00");
     private int second;
     private int minute;
+//    private final int pivotX = 200;
+//    private final int pivotY = 200;
+//    private int moveX = 200;
+//    private int moveY = 0;
+//    private final int maxDistanceSquare = 40000;
+    private JButton start = new JButton("Start");
+    private JButton reset = new JButton("Reset");
+    private JButton stop = new JButton("Stop");
+    private JLabel time = new JLabel(fmt.format(minute)+ " : " + fmt.format(second));
+    private Timer minuteTimer = new Timer(60000, new MinuteStopWatchListener());
+    private Timer secondTimer = new Timer(1000, new SecondStopWatchListener());
 
     public StopWatchPanel() {
         setBackground(Color.white);
         setPreferredSize(new Dimension(400, 500));
         
-        JButton start = new JButton("Start");
-        JButton reset = new JButton("Reset");
-        JButton stop = new JButton("Stop");
-        JLabel time = new JLabel("hahahah");
-        Timer timer = new Timer(second, null);
+        time.setFont(new Font("Helveltica", Font.PLAIN, 36));
+        start.addActionListener(new ButtonListener());
+        stop.addActionListener(new ButtonListener());
+        reset.addActionListener(new ButtonListener());
+
         
-        add(Box.createRigidArea(new Dimension(400, 400)));
+        add(Box.createRigidArea(new Dimension(400, 390)));
         add(time);
-        add(Box.createRigidArea(new Dimension(400, 20)));
+        add(Box.createRigidArea(new Dimension(400, 5)));
         add(start);
         add(reset);
         add(stop);
@@ -40,9 +56,45 @@ public class StopWatchPanel extends JPanel{
         
         g.setColor(Color.white);
         g.fillOval(195, 195, 10, 10);
+        
+//        g.drawLine(pivotY, pivotY, moveX, moveY);
     }
     
-    public void Timer() {
-        
+    private class SecondStopWatchListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            second++;
+            time.setText(fmt.format(minute)+ " : " + fmt.format(second));
+        }
     }
+    
+    private class MinuteStopWatchListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            minute++;
+            time.setText(fmt.format(minute)+ " : " + fmt.format(second));
+        }
+    }
+    
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+
+            if (event.getSource() == start) {
+                minuteTimer.start();
+                secondTimer.start();
+            }
+            if (event.getSource() == reset) {
+                minuteTimer.stop();
+                secondTimer.stop();
+                minute = 0;
+                second = 0;
+                time.setText(fmt.format(minute)+ " : " + fmt.format(second));
+            }
+            if (event.getSource() == stop) {
+                minuteTimer.stop();
+                secondTimer.stop();
+            }
+            
+        }
+    }
+    
+    
 }
